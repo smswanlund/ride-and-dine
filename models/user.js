cee78c4eb539357cc6c0da370ca4310198958da7
smswanlund <muelmac@gmail.com>
2020-01-08 13:19:42 -0500

    updated user model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//structure of user account
+const userSchema = new Schema({
+  fullName: { type: String, required: true },
+  userName: { type: String, required: true },
+  password: { type: String, required: true },
+  age: {type: Number, required: true},
+  liked: [String],
+  disliked: [String]
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
