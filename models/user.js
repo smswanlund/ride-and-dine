65c3602ba721412028853aabdce32bc0f2a663f1
smswanlund <muelmac@gmail.com>
2020-01-09 21:28:39 -0500

    initial mongo connection (not working yet)

--- models/user.js
+++ models/user.js
@@ -3,8 +3,10 @@ const Schema = mongoose.Schema;
 
 //structure of user account
 const userSchema = new Schema({
-  fullName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: {type: String, required: true},
   userName: { type: String, required: true },
+  email: {type: String, required: true},
   password: { type: String, required: true },
   age: {type: Number, required: true},
   liked: [String],
