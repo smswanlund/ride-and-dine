8fb2d261f3bbf05a5225a95347f7b2ffda88fc1f
Gage D'Orlando <gagedorlando@gmail.com>
2020-01-27 19:48:29 -0500

    route fix

--- client/src/pages/404.js
+++ client/src/pages/404.js
@@ -1,23 +1,21 @@
 import React from "react";
-import { Col, Row, Container } from "../components/Grid";
-import Jumbotron from "../components/Jumbotron";
 
 function NoMatch() {
   return (
-    <Container fluid>
-      <Row>
-        <Col size="md-12">
-          <Jumbotron>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+          <div className="jumbotron">
             <h1>404 Page Not Found</h1>
             <h1>
               <span role="img" aria-label="Face With Rolling Eyes Emoji">
                 🙄
               </span>
             </h1>
-          </Jumbotron>
-        </Col>
-      </Row>
-    </Container>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
